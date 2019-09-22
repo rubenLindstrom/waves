030507a6dc719eae4bc9c77d360493472bfd48b9
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-09-22 08:47:23 +0200

    Created user model

--- server/server.js
+++ server/server.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//Models
+const { User } = require("./models/user");
+
+//============================================
+//                USERS
+//============================================
+
+app.post("/api/users/register", (req, res) => {
+  res.status(200);
+});
 
 const port = process.env.PORT || 3000;
 
