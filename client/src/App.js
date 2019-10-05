d5e47f1d3d1ebeaf4e802bb544998a66c806bf5b
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-10-05 21:53:24 +0200

    added model article and initiated client side

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import axois from "axois";
+
+function App() {
+  return <div className="App">MY APP</div>;
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -17,10 +17,64 @@ app.use(cookieParser());
 const { User } = require("./models/user");
 const { Brand } = require("./models/brand");
 const { Wood } = require("./models/wood");
+const { Article } = require("./models/article");
 
 // Middleware
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
+
+//============================================
+//                ARTICLES
+//============================================
+
+// BY SOLD
+// /api/product/articles?sortBy=sold&order=desc&limit=4
+app.get("/api/product/articles", (req, res) => {
+  const order = req.query.order || "asc";
+  const sortBy = req.query.sortBy || "_id";
+  const limit = parseInt(req.query.limit) || 100;
+
+  Article.find({})
+    .populate("brand")
+    .populate("wood")
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.send(articles);
+    });
+});
+
+// /api/product/articles_by_id?id=123,456?type=array
+app.get("/api/product/articles_by_id", (req, res) => {
+  const type = req.query.type;
+  let items = req.query.id;
+
+  if (type === "array") {
+    const ids = req.query.id.split(",");
+    items = ids.map(el => {
+      return mongoose.Types.ObjectId(el);
+    });
+  }
+  Article.find({ _id: { $in: items } })
+    .populate("brand")
+    .populate("wood")
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });
+});
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const article = new Article(req.body);
+
+  article.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc
+    });
+  });
+});
 
 //============================================
 //                WOODS
