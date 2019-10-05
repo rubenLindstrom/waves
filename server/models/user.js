5d0c1e2de0d5cae00ca9356b3c66113105e02374
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-10-05 19:44:25 +0200

    Added middleware admin and auth, and models brand and wood

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SALT_I = 10;
+require("dotenv").config();
 
 const userSchema = mongoose.Schema({
   email: {
@@ -38,6 +42,53 @@ const userSchema = mongoose.Schema({
     type: String
   }
 });
+
+// Vi säger att PRE "save" (dvs. innan en användare sparas) så ska vi utföra en handling. next betyder "fortsätt vad du höll på med"
+// Av nån anledning så funkar inte ES6 fat arrow functions
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(SALT_I, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function(callback) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function(token, callback) {
+  const user = this;
+
+  jwt.verify(token, process.env.SECRET, function(err, decode) {
+    user.findOne({ _id: decode, token: token }, function(err, user) {
+      if (err) return callback(err);
+      callback(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
